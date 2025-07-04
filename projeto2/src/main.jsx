46f072ff086b9cbbd65c1e920da9824a7c745290
Anderson D Bernardo <anderson.danttas@hotmail.com>
2025-07-03 21:21:48 -0300

    Update

--- projeto2/src/main.jsx
+++ projeto2/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Navbar, SubNavbar } from './components/Navbar.jsx'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-    <SubNavbar />
+    <App />
   </StrictMode>,
 )
---
+++ projeto2/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <a href="/">Inicial</a> <br/>
+        <a href="/sobre">Sobre</a> <br/>
+        <a href="/contato">Contato</a> <br/>
+    </div>
+    
+  )
+}
